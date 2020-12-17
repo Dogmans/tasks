b4b97413bd7714bbd5d2940a9f60c2e91f49b7f1
Will Frank-Gemmill <will.frank-gemmill@airex.tech>
2020-12-17 11:01:58 +0000

    Split out tests into model and API tests

--- tasks/web/tests.py
+++ tasks/web/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase, Client
 from web.models import Queue
 
 
-class TestApi(TestCase):
+def TestBase(TestCase):
 
 	def setUp(self):
 		super().setUp()
@@ -14,13 +14,42 @@ class TestApi(TestCase):
 			first_name="Test",
 			last_name="User",
 		)
+		self._client = Client()
+
+
+class TestModels(TestBase):
+	'''
+	Holds tests that create and update models directly
+	Used for preliminary functionality tests before we try to 
+	address via the API
+	'''
+
+	def test_create_queue(self):
 		test_queue = Queue(
 			owner = self._user,
 			title = "TestQueue"
 		)
-		test_queue.save()# TODO - create queue, slots etc.
+		self.assertTrue(Queue.objects.all().count())
 
-	def test_get_queues(self):
-		client = Client()
-		result = client.get("/api/queues/")
+
+class TestApi(TestBase):
+	'''
+	Holds tests that call the REST API to perform functions 
+	'''
+
+	def test_create_queue(self):
+		# TODO - actually make a Queue via a request
+		result = self._client.get("/api/queues/")
 		self.assertTrue(result.data)
+
+	def test_create_populate_queue(self):
+		'''
+		Create a queue with slots and tasks and retrieve via REST
+		'''
+		pass
+
+	def test_create_insert_queue(self):
+		'''
+		Create a queue then insert some task in the middle of it and retrieve
+		'''
+		pass
