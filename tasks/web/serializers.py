f64a062a633d0c33ee44d31733818d60c2801824
Will Frank-Gemmill <will.frank-gemmill@airex.tech>
2020-12-20 22:43:22 +0000

    updated Queue serializer with ID

--- tasks/web/serializers.py
+++ tasks/web/serializers.py
@@ -15,4 +15,4 @@ class UserSerializer(serializers.ModelSerializer):
 class QueueSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Queue
-		fields = ["title"]
+		fields = ["id", "title"]
